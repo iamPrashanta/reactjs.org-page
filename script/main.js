51c9799d9aa557637627425d720a2c1661095d0d
Prashanta mondal <iloveprashanta@gmail.com>
2020-11-23 11:52:31 +0530

    upload file

---
+++ script/main.js
@@ -0,0 +1,28 @@
+$('.sidenav ul').click(function () {
+    if ($(this).hasClass('active-content')) {
+        if ($(this).sibilings().hasClass('active-content')) {
+            //
+        } else {
+            $(this).removeClass('active-content');
+        }
+    } else {
+        $('.sidenav ul').removeClass('active-content');
+        $(this).addClass('active-content');
+    }
+});
+$('.sidenav ul li a').click(function () {
+    if ($(this).hasClass('active-item')) {
+        if ($(this).sibilings().hasClass('active-item')) {
+            //
+        } else {
+            $(this).removeClass('active-item');
+        }
+    } else {
+        $('.sidenav ul li a').removeClass('active-item');
+        $(this).addClass('active-item');
+    }
+});
+
+$('.fa-bars').click(function () {
+    alert('I am perfectly working (code was missing)');
+})
